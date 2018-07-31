19984d5166ec607ba76f02228d297a5b7a5a9422
Kevin Alexander Argueta <kevin_argueta@alumni.brown.edu>
2018-07-31 14:46:46 +0000

    Now version 8 able to delete selected items and toggle selected items

--- version5.js
+++ version5.js
@@ -30,12 +30,13 @@ var todoList = {
         this.displayTodos();
     },
     
-    // deleteTodo: function(position){
-    //     this.splice(position,1);
-    //     this.displayTodos();
-    // }
+    deleteTodo: function(position){
+        this.todos.splice(position,1);
+        this.displayTodos();
+    },
+    
     toggleCompleted: function(position){
-        this.todos[position].completed = !this.todos[position].completed;
+        this.todos[position].completed = !(this.todos[position].completed);
         this.displayTodos();
     },
     
@@ -70,13 +71,47 @@ var todoList = {
 // todoList.toggleAll();
 // todoList.toggleAll();
 
-var displayTodosButton = document.getElementById('displayTodosButton');
-var toggleAllButton = document.getElementById('toggleAllButton');
+// var displayTodosButton = document.getElementById('displayTodosButton');
+// var toggleAllButton = document.getElementById('toggleAllButton');
 
-displayTodosButton.addEventListener('click', function(){
-    todoList.displayTodos();
-});
+// displayTodosButton.addEventListener('click', function(){
+//     todoList.displayTodos();
+// });
 
-toggleAllButton.addEventListener('click', function(){
-    todoList.toggleAll();
-});
+// toggleAllButton.addEventListener('click', function(){
+//     todoList.toggleAll();
+// });
+
+
+var handlers = {
+    displayTodos: function(){
+        todoList.displayTodos();
+    },
+    toggleAll: function(){
+        todoList.toggleAll();
+    },
+    addTodo: function(){
+        var addTodoTextInput = document.getElementById('addTodoTextInput');
+        todoList.addTodo(addTodoTextInput.value);
+        addTodoTextInput.value = '';
+    },
+    changeTodo: function(){
+        var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+        var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+        todoList.changeTodo(changeTodoPositionInput.valueAsNumber,changeTodoTextInput.value);
+        changeTodoPositionInput.value = '';
+        changeTodoTextInput.value = '';
+    },
+    
+    deleteTodo: function(){
+        var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
+        todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+        deleteTodoPositionInput.value = '';
+    },
+    
+    toggleCompleted: function(){
+        var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+        todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+        toggleCompletedPositionInput.value = '';
+    }   
+}
